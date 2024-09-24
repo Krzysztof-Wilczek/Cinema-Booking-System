440f3d7362de8d7024b2fd67574235ee4922cea7
Krzysztof_W <AQ243707@aquinas.ac.uk>
2024-09-24 10:28:35 +0100

    Add project files.

---
+++ Cinema Booking System/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Aquinas Multiplex");
+Console.WriteLine("We are presently showing: ");
+Console.WriteLine("1. Teenage Horror film (15) ");
+Console.WriteLine("2. How I Live Now (15) ");
+Console.WriteLine("3. Another Marvel Film (12) ");
+Console.WriteLine("4. Filth (18) ");
+Console.WriteLine("5. Planes (U) ");
+int[] films = { 1, 2, 3, 4, 5 };
+int[] AgeRequired = { 15, 15, 12, 18, 0 };
+
